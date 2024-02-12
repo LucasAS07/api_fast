26851569f076bf231a265a9416e9b09c6f57d4f0
Lucas Rodrigues <71413589+LucasAS07@users.noreply.github.com>
2024-02-12 14:34:40 -0300

    Protege os endpoints PUT e DELETE com autenticação

--- tests/test_app.py
+++ tests/test_app.py
@@ -19,6 +19,20 @@ def test_create_user(client):
     }
 
 
+# def test_create_user_already_registered(client, user):
+#     response = client.post(
+#         '/users/',
+#         json={
+#             'username': 'Teste',
+#             'email': 'test@test.com',
+#             'password': 'testtest',
+#         },
+#     )
+
+#     assert response.status_code == 400
+#     assert response.json() == {'detail': 'Username already registered'}
+
+
 def test_read_users(client):
     response = client.get('/users/')
     assert response.status_code == 200
@@ -31,13 +45,14 @@ def test_read_users_with_users(client, user):
     assert response.json() == {'users': [user_schema]}
 
 
-def test_update_user(client, user):
+def test_update_user(client, user, token):
     response = client.put(
-        '/users/1',
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
         json={
             'username': 'bob',
             'email': 'bob@example.com',
-            'password': 'mynewpassoword',
+            'password': 'mynewpassword',
         },
     )
     assert response.status_code == 200
@@ -48,7 +63,22 @@ def test_update_user(client, user):
     }
 
 
-def test_delete_user(client, user):
-    response = client.delete('/users/1')
+def test_delete_user(client, user, token):
+    response = client.delete(
+        f'/users/{user.id}',
+        headers={'Authorization': f'Bearer {token}'},
+    )
     assert response.status_code == 200
     assert response.json() == {'message': 'User deleted'}
+
+
+def test_get_token(client, user):
+    response = client.post(
+        '/token',
+        data={'username': user.email, 'password': user.clean_password},
+    )
+    token = response.json()
+
+    assert response.status_code == 200
+    assert 'access_token' in token
+    assert 'token_type' in token
